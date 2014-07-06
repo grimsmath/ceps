0d8a075723be2fd19d4624767f977cec7044f328
David King <grimsmath@gmail.com>
2014-07-05 22:17:03 -0400

    Added requirements tab to course show view

--- app/models/predict.rb
+++ app/models/predict.rb
@@ -1,7 +1,13 @@
 class Predict
 
-  def predict_enrollment(course, start_semester_id, end_semester_id)
+  def self.predict_enrollment(course, start_semester_id, end_semester_id)
 
+    start_semester = Semester.where(id: start_semester_id).first
+    end_semester = Semester.where(id: end_semester_id).first
+
+    prerequisites = course.requirements
+
+    return start_semester, end_semester, prerequisites
   end
 
 end
