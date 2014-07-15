aa8b3d78d55f44ee70a258805c81662220b3f145
David King <grimsmath@gmail.com>
2014-07-15 11:50:57 -0400

    Added requirement weighting

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -2,6 +2,7 @@ class Requirement < Content
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :course_id, type: String
+  field :course_id,     type: String
   field :corequirement, type: Boolean
+  field :weight,        type: Float,    default: 0.5
 end
